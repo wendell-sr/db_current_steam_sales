76ac3bd73cb4b8be59dad5a327514b9ab84b1a10
Wendell Marques <81488797+wendell-sr@users.noreply.github.com>
2024-11-09 00:23:47 -0300

    Update setup_bigquerry.py

--- src/setup_bigquerry.py
+++ src/setup_bigquerry.py
@@ -1,16 +1,34 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
+
 credentials = service_account.Credentials.from_service_account_file(
     'config/credentials.json'
 )
 
+
 client = bigquery.Client(credentials=credentials)
 
-# Cria o dataset
+
 dataset_id = f"{client.project}.steam_sales"
 dataset = bigquery.Dataset(dataset_id)
-dataset.location = "US"  # Especifica a localização do dataset
+dataset.location = "US"
 dataset = client.create_dataset(dataset, exists_ok=True)
-
 print(f"Dataset {dataset_id} criado com sucesso.")
+
+
+table_id = f"{dataset_id}.sales"
+table = bigquery.Table(table_id)
+table.schema = [
+    bigquery.SchemaField("App_ID", "STRING"),
+    bigquery.SchemaField("Image_URL", "STRING"),
+    bigquery.SchemaField("Name", "STRING"),
+    bigquery.SchemaField("Discount", "STRING"),
+    bigquery.SchemaField("Price", "STRING"),
+    bigquery.SchemaField("Rating", "STRING"),
+    bigquery.SchemaField("Release_Date", "STRING"),
+    bigquery.SchemaField("Ends", "STRING"),
+    bigquery.SchemaField("Starts", "STRING")
+]
+table = client.create_table(table, exists_ok=True)
+print(f"Tabela {table_id} criada com sucesso.")
